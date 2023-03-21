6238ba0d1eff47cec9c4859f2f8cf7e7fcbf8f6f
Proccyon <36238733+Proccyon@users.noreply.github.com>
2023-03-21 18:12:15 +0100

    Added robot physics

--- PythonVersion/Main.py
+++ PythonVersion/Main.py
@@ -5,15 +5,101 @@
 import pyglet
 from pyglet import shapes
 import glm
+import numpy as np
 
 
 #-----Methods-----#
+
+class Joint():
+    
+    def __init__(self, parentBar, childBar, PositionOffset, rotationOffset):
+        self.parentBar = parentBar
+        self.childBar = childBar
+        self.PositionOffset = PositionOffset
+        self.rotationOffset = rotationOffset
+        
+        
+    def getJointFrame(self):
+        
+        parentPosition, parentAngle = self.parentBar.getFrame()        
+        parentDirection = vec(np.cos(parentAngle), np.sin(parentAngle))
+        
+        jointPosition = parentPosition + (self.parentBar.length - self.positionOffset) * parentDirection
+        jointAngle = parentAngle + self.rotationOffset
+        
+        return jointPosition, jointAngle
+        
+        
+    def getChildFrame(self):
+        
+        pass
+        
+        
+        
+
+class RotatingJoint(Joint):
+    
+    
+    def __init__(self, parentBar, childBar, positionOffset, rotationOffset, theta):
+        
+        self.theta = theta
+        Joint.__init__(self, parentBar, childBar, positionOffset, rotationOffset)
+    
+    def getChildFrame(self):
+        
+        jointPosition, jointAngle = self.getJointFrame()
+        
+        return jointPosition, jointAngle + self.theta
+    
+    
+    
+        
+class ExtendingJoint(Joint):
+
+    def __init__(self, parentBar, childBar, positionOffset, rotationOffset, minLength, maxLength, length):
+        
+        self.minLength = minLength
+        self.maxLength = maxLength
+        self.length = length
+        Joint.__init__(self, parentBar, childBar, positionOffset, rotationOffset)
+        
+    def getChildFrame(self):
+        
+        jointPosition, jointAngle = self.getJointFrame()    
+        jointDirection = vec(np.cos(jointAngle), np.sin(jointAngle))
+        
+        return jointPosition + self.length * jointDirection, jointAngle
+    
+
+class Bar():
+    
+    def __init__(self, parentJoint, childJoint, length):
+        self.parentJoint = parentJoint
+        self.childJoint =  childJoint
+        self.length = length
+        
+    def getFrame(self):
+        
+        return self.parentJoint.getChildFrame()
+        
+
+
+
 
 class vec():
     
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        
+    def __add__(self, vec2):
+        return vec(self.x + vec2.x, self.y + vec2.y)
+    
+    def __sub__(self, vec2):
+        return vec(self.x - vec2.x, self.y - vec2.y)
+        
+    def __mul__(self, vec2):
+        return vec(self.x * vec2.x, self.y * vec2.y)
 
 
 def drawRobot(batch, P0, direction, theta1, theta2, theta3):
